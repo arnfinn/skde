a3922dab1d831f51ca3e9c3031168b7219701f6e
Arnfinn <arnfinn.steindal@gmail.com>
2017-11-07 13:09:58 +0100

    latex probably working again

--- _build.sh
+++ _build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 
-Rscript -e "rmarkdown::render_site(output_format = c('bookdown::gitbook'), encoding = 'UTF-8')"
+#Rscript -e "rmarkdown::render_site(output_format = c('bookdown::gitbook'), encoding = 'UTF-8')"
 
-#Rscript -e "rmarkdown::render_site(output_format = c('bookdown::gitbook', 'bookdown::pdf_book', 'bookdown::epub_book'), encoding = 'UTF-8')"
+Rscript -e "rmarkdown::render_site(output_format = c('bookdown::gitbook', 'bookdown::pdf_book', 'bookdown::epub_book'), encoding = 'UTF-8')"
