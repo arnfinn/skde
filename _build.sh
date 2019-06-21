97da6782161d98a58ebbff0cf62392d5859d79e3
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2019-06-21 12:31:40 +0200

    Fix build (#12)
    do not make pdf/epub

--- _build.sh
+++ _build.sh
@@ -2,6 +2,6 @@
 
 export PATH=~/pandoc/1.19/usr/bin/:$PATH
 
-#Rscript -e "rmarkdown::render_site(output_format = c('bookdown::gitbook'), encoding = 'UTF-8')"
+Rscript -e "rmarkdown::render_site(output_format = c('bookdown::gitbook'), encoding = 'UTF-8')"
 
-Rscript -e "rmarkdown::render_site(output_format = c('bookdown::gitbook', 'bookdown::pdf_book', 'bookdown::epub_book'), encoding = 'UTF-8')"
+#Rscript -e "rmarkdown::render_site(output_format = c('bookdown::gitbook', 'bookdown::pdf_book', 'bookdown::epub_book'), encoding = 'UTF-8')"
